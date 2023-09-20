58edb57fc5dba8679d20d41d19cb0acb2536dd39
AlejoZarate77 <zaratealejo26@gmail.com>
2023-09-20 08:40:05 -0300

    Third commit

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './img/Calculator_30001.png'
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className='App'>
+      <div className='logo-conteiner'>
+        <img
+          src={Logo}
+          className='Logo'
+          alt='img logo' />
+      </div>
     </div>
   );
 }
